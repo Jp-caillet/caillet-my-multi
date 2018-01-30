5b8fb87e9c50a6b69121ea8405c86b5cfc0b1918
caillet <jp78920@hotmail.com>
2018-01-30 10:20:18 +0100

    feat(multi argument)

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const number = 1;
-const max = 10;
-
-for (let i=1;i<=max;i+=1 ) {
-    Console.log(`${number} X ${i} = ${(i*max)}`);
+const multi = (number, max)=>{
+	for (let i=1; i<=max; i+=1 ) {
+    	console.log(`${number} X ${i} = ${(i*max)}`);
+	}
+}
+const number = parseInt(process.argv[2]);
+const max = parseInt(process.argv[3]);
+if (number && max) {
+	multi(number, max);
+}else{
+	console.log('[error]: multi <number> <number>');
 }
